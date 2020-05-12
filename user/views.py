2a2cad635af16a7802c5fc9e97ff123ff1c96f42
fridrik <fridriks19@ru.is>
2020-05-12 17:01:50 +0000

    backup

--- user/views.py
+++ user/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+from user.forms.profile_form import ProfileForm
+from user.models import Profile
+from cart.models import Order
+
+
 def register(request):
     if request.method =='POST':
         form = UserCreationForm(data=request.POST)
@@ -12,9 +17,19 @@ def register(request):
     })
 
 def profile(request):
-    profile = Profile.objects.filter(user=request.user)
+    profile = Profile.objects.filter(user=request.user).first()
+    user_orders = Order.objects.filter(is_ordered=True, owner=profile)
+    context = {
+        'user_orders': user_orders
+    }
+
     if request.method == 'POST':
-        print(1)
-    return render(request, 'user/profile.html', {
-        'form': ''
+        form = ProfileForm(instance=profile, data=request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    return render(request, 'user/profile.html', context, {
+        'form': ProfileForm(instance=profile)
     })
